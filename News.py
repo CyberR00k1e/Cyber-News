a4abd7292550d4cc1069261b31a8a55b5d3dfa7f
srt <sharat.nautiyal@gmail.com>
2023-02-03 16:25:46 +0800

    corrected the loop to show the news in the same height

--- News.py
+++ News.py
@@ -5,44 +5,63 @@ import streamlit as st
 today = datetime.date.today()
 apikey=os.getenv("apikey")
 
-url = f"https://newsapi.org/v2/everything?q='cyberattack'&from={today}&sortBy=publishedAt&apiKey={apikey}"
+url = f"https://newsapi.org/v2/everything?q='cyberattack'&from=2023-02-02&sortBy=publishedAt&apiKey={apikey}"
 response = requests.get(url, verify=False).json()
 length = len(response["articles"])
+print(length)
 
 
 st.title("Today's News About Cyber Attacks")
 y=1
-col1, col2, col3 = st.columns(3,gap="large")
-for i in range(length):
-    news = response["articles"][i]
-    title = news["title"]
-    description = news["description"]
-    url = news["url"]
-    image = news["urlToImage"]
-    content = news["content"]
 
-    if y==1:
-        with col1:
+#col1, col2, col3,col4, col5, col6 = st.columns([4,2,4,2,4,2])
+
+new_i_value=0
+for i in range(length):
+    i=new_i_value
+    col1, col2, col3, col4, col5, col6 = st.columns([4, 2, 4, 2, 4, 2])
+    for repeat in range(3):
+        print(i)
+        news = response["articles"][i]
+        title = news["title"]
+        description = news["description"]
+        url = news["url"]
+        image = news["urlToImage"]
+        content = news["content"]
+
+        if y==1:
+            with col1:
                 st.header(title)
                 st.write(description)
                 st.write(url)
-
-    elif y==2:
-        with col2:
-            st.header(title)
-            st.write(description)
-            st.write(url)
-
-    else:
-        with col3:
-            st.header(title)
-            st.write(description)
-            st.write(url)
-    y=y+1
-    if y >3:
-        y=1
+            with col2:
+                st.empty()
 
 
+        elif y==2:
+            with col3:
+                st.header(title)
+                st.write(description)
+                st.write(url)
+            with col4:
+                st.empty()
+
+
+        else:
+            with col5:
+                st.header(title)
+                st.write(description)
+                st.write(url)
+            with col6:
+                st.empty()
+
+        y=y+1
+
+        if y >3:
+            y=1
+        i=i+1
+    new_i_value=i
+    print(f"the loop is {new_i_value}")
 
 
 
